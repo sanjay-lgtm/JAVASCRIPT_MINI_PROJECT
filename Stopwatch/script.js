3aeb8c297390240f561c196e74cbefd8092aa549
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-03-14 19:55:04 +0530

    new Project

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,103 +1,47 @@
-// window.onload = function () {
-//     var seconds = 00;
-//     var tens = 00;
-//     var appendTens = document.getElementById("tens");
-//     var appendSeconds = document.getElementById("seconds")
-//     var buttonstart = document.getElementById("button-start")
-//     var buttonstop = document.getElementById("button-stop")
-//     var buttonreset = document.getElementById("button-reset")
-//     var Interval;
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
 
-//     buttonstart.onclick = function () {
-//         Interval = setInterval(startTimer, 10);
-//     }
+let msec = 00;
+let secs = 00;
+let mins = 00;
 
-//     buttonstop.onclick = function(){
-//         clearInterval(Interval);
-//     }
+let timerId = null;
 
-//     buttonreset.onclick = function(){
-//         clearInterval(Interval);
-//         tens = '00';
-//         seconds = "00";
-//         appendTens.innerHTML = tens;
-//         appendSeconds.innerHTML = seconds;
-//     }
-//     function startTimer() {
-//         tens++;;
-//         if (tens <= 9) {
-//             appendTens.innerHTML = "0" + tens;
-//             console.log(tens + "One");
-//         }
-
-//         if (tens > 9) {
-//             appendTens.innerHTML = tens;
-//             console.log(tens + "Two");
-//         }
-
-//         if (tens > 99) {
-
-//             console.log(tens + "Third");
-//             seconds++;
-//             appendSeconds.innerHTML = "0" + seconds;
-//             tens = 0;
-//             appendTens.innerHTML = "0" + 0;
-//         }
-
-//         if (tens > 9) {
-//             appendSeconds.innerHTML = seconds;
-//             console.log(tens + "Four");
-//         }
-
-//     }
-// }
-
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let timeRef = document.querySelector(".timer-display");
-let int = null;
-
-document.getElementById("button-start").addEventListener("click", () => {
-    if(int !== null) {
-        clearInterval(int);
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
     }
-    int = setInterval(displayTimer, 10);
+    timerId = setInterval(startTimer, 10);
 });
 
-document.getElementById("button-pause").addEventListener("click", () => {
-    clearInterval(int);
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
 });
 
-document.getElementById("button-reset").addEventListener("click", () => {
-    clearInterval(int);
-    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-    timeRef.innerHTML = "00 : 00 : 00 : 000 ";
-}); 
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 00;
+});
 
-function displayTimer() {
-    milliseconds += 10;
-    if(milliseconds == 1000) {
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60) {
-            seconds = 0;
-            minutes++;
-            if(minutes == 60) {
-                minutes = 0;
-                hours++;
-            }
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+mins++;
         }
     }
 
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = 
-        milliseconds < 10
-        ? "00" + milliseconds
-        : milliseconds < 100
-        ? "0" + milliseconds
-        : milliseconds;
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
 
-    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
 
 }
