44e384dec542584719a2693bfb2988769ed7ea2c
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-03-17 23:33:44 +0530

    change

--- Iphone_weekly_test/script.js
+++ Iphone_weekly_test/script.js
@@ -57,26 +57,3 @@ document.addEventListener("DOMContentLoaded", () => {
     
     
     
-    // ---used code
-       // for(let i=1;i<=12;i++){
-        //     let mobile_card = document.createElement("div");
-        //     mobile_card.style.height = "520px";
-        //     mobile_card.style.width = "350px";
-        //     mobile_card.style.display = "flex";
-        //     mobile_card.style.flexDirection = "column";
-        //     mobile_card.style.alignItems = "center";
-        //     mobile_card.style.justifyItems = "space-evenly";
-        //     mobile_card.style.gap = "10px";
-        //     mobile_card.style.borderRadius = "20px";
-        //     mobile_card.style.boxShadow = "0 0 20px rgb(100,25,230)";
-     
-        //     mobile_card.innerHTML = `
-        //     <img class="mt-[20px] w-[250px]" src="${data.data[i].image}">
-        //     <h2 class="">${data.data[1].brand} ${data.data[i].phone_name}</h2>
-        //     <p class="px-4 text-white text-center">There are many variations of passages of available, but the majority have suffered
-        //     </p>
-        //     <button id="showDetailBtn" class="px-4 py-2 rounded-xl bg-[rgb(100,25,230)] text-white">SHOW DETAILS<button>
-        //     `;
-        //     // console.log(data.data[i].image,data.data[1].brand,data.data[i].phone_name);
-        //     mobile_Container.append(mobile_card);        
-        // }
